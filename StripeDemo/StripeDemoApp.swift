00a3018071b3c643afa261d1ed1bd5014a87eb2c
Sameer Nadaf <nsameernadaf@gmail.com>
2026-02-12 23:33:26 +0530

    Initial commit

---
+++ StripeDemo/StripeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StripeDemoApp.swift
+//  StripeDemo
+//
+//  Created by Sameer Nadaf on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct StripeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
