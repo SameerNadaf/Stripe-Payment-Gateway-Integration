6e3a38a9426e623e56df72ab0b739e5c3585ea7c
Sameer Nadaf <nsameernadaf@gmail.com>
2026-03-01 15:30:11 +0530

    Add app-level Stripe configurations

--- StripeDemo/StripeDemoApp.swift
+++ StripeDemo/StripeDemoApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import StripePaymentSheet
 
 @main
 struct StripeDemoApp: App {
+    init() {
+        // Replace with your actual Stripe Publishable Key
+        StripeAPI.defaultPublishableKey = "pk_test_51T64AZCatVDdRsnlcgI0hxozIQVCxhEOzq0EFWdRPMGhBdKev0uXMECr8jJIZPgmzG9qgwdGHuK4rJbQhFhVzgXU00Gzgt9dtG"
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
